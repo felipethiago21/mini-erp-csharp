3f86bf12a4feaf3fab768c6fbac473be3174748e
Felipe Thiago <felipethiagorx@gmail.com>
2026-07-13 10:00:16 -0300

    feat: primeira versão do Mini ERP em C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using MiniErp.Services;
+
+var produtoService = new ProdutoService();
+
+Console.Write("Digite o nome do produto: ");
+var nome = Console.ReadLine();
+
+Console.Write("Digite o preço do produto: ");
+var preco = Console.ReadLine();
+
+Console.Write("Digite o Estoque do produto: ");
+var estoque = Console.ReadLine();
+
+var produtoCadastrado = produtoService.Cadastrar(
+    nome ?? string.Empty,
+    decimal.Parse(preco ?? "0"),
+    int.Parse(estoque ?? "0")
+);
+
+Console.WriteLine();
+Console.WriteLine($"Produto: {produtoCadastrado.Nome}");
+Console.WriteLine($"Preço: {produtoCadastrado.Preco:C}");
+Console.WriteLine($"Estoque: {produtoCadastrado.QuantidadeEstoque}");
---
+++ Services/ProdutoService.cs
@@ -0,0 +1,42 @@
+using MiniErp.Models;
+namespace MiniErp.Services;
+
+public class ProdutoService
+{
+    private readonly List<Produto> _produtos = [];
+    private int _proximoId = 1;
+
+    public Produto Cadastrar(string nome, decimal preco, int estoqueInicial)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new ArgumentException("O nome é obrigatório.");
+
+        if (preco <= 0)
+            throw new ArgumentException("O preço deve ser maior que zero.");
+
+        if (estoqueInicial < 0)
+            throw new ArgumentException("O estoque não pode ser negativo.");
+
+        var produto = new Produto
+        {
+            Id = _proximoId++,
+            Nome = nome,
+            Preco = preco,
+            QuantidadeEstoque = estoqueInicial
+        };
+
+        _produtos.Add(produto);
+
+        return produto;
+    }
+
+    public List<Produto> Listar()
+    {
+        return _produtos;
+    }
+
+    public Produto? BuscarPorId(int id)
+    {
+        return _produtos.FirstOrDefault(produto => produto.Id == id);
+    }
+}
